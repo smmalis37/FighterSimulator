a96c542a2a23ef3bfc781abf936e28f2e93fc397
Steven Malis <smmalis37@gmail.com>
2018-04-25 22:19:56 -0700

    Add user facing input and validation.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,10 +3,65 @@ extern crate fighter_simulator;
 use fighter_simulator::*;
 
 fn main() {
-    let f1 = Fighter::new("Alice".to_owned(), 8, 2, 4, 140).unwrap();
-    let f2 = Fighter::new("Bob".to_owned(), 7, 3, 4, 125).unwrap();
+    let f1 = get_fighter();
+    println!("{} successfully registered.", f1.name);
+    println!();
+    let f2 = get_fighter();
+    println!("{} successfully registered.", f2.name);
+    println!();
 
     Fight::new(&f1, &f2).run_with_reporting(report_handler);
+}
+
+fn get_fighter() -> Fighter {
+    loop {
+        let name = get_value("Enter the fighter's name:");
+        let attack = get_value("Enter the fighter's attack:");
+        let speed = get_value("Enter the fighter's speed:");
+        let endurance = get_value("Enter the fighter's endurance:");
+        let max_health = get_value("Enter the fighter's max health:");
+
+        match Fighter::new(name, attack, speed, endurance, max_health) {
+            Ok(fighter) => break fighter,
+            Err(e) => match e {
+                FighterStatError::IncorrectPointTotal(total) => println!(
+                    "That build uses {} points. You must use exactly {} points.",
+                    total, TOTAL_POINTS
+                ),
+                FighterStatError::ZeroStat(stat) => {
+                    println!("{:?} values of zero are not allowed.", stat)
+                }
+                FighterStatError::StatAboveMax(stat) => println!(
+                    "{:?} values above {} are not allowed.",
+                    stat,
+                    stat.costs().len() - 1
+                ),
+                FighterStatError::HealthBelowBase => println!(
+                    "{} is below the base health value of {}.",
+                    max_health, BASE_HEALTH
+                ),
+                FighterStatError::HealthNotCleanlyDivisible => println!(
+                    "{} is not cleanly divisible by the health per points value of {}.",
+                    max_health, HEALTH_PER_POINT
+                ),
+            },
+        }
+    }
+}
+
+fn get_value<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        let mut buffer = String::new();
+        println!("{}", prompt);
+        let read_attempt = std::io::stdin()
+            .read_line(&mut buffer)
+            .map(|_| buffer.trim().parse());
+        if let Ok(Ok(value)) = read_attempt {
+            break value;
+        } else {
+            println!("Invalid input.");
+        }
+    }
 }
 
 fn report_handler(report: &Report) {
